96992568c5659088e020d5f4a13f3c3a117e7af8
Tito Lucca Oliveira <titoluccao@gmail.com>
2026-04-30 16:02:56 -0300

    feat: integrar db com /applications

--- apps/web/src/app/(auth)/applications/page.tsx
+++ apps/web/src/app/(auth)/applications/page.tsx
@@ -1,4 +1,7 @@
 import { MaxWidth } from "@/components/max-width";
+import { auth } from "@prosopopeia/auth";
+import { db, eq } from "@prosopopeia/db";
+import { application } from "@prosopopeia/db/schema/index";
 import { Button } from "@prosopopeia/ui/components/button";
 import {
   Card,
@@ -8,43 +11,30 @@ import {
 } from "@prosopopeia/ui/components/card";
 import { generate } from "geopattern";
 import { ArrowRightIcon } from "lucide-react";
+import { headers } from "next/headers";
 import Link from "next/link";
 import { AddApplicationModal } from "./add-application-modal";
 
-const applicationMock = [
-  {
-    id: "1",
-    name: "Desenvolvedor Full-Stack",
-    company: "Acme corp",
-    createdAt: new Date(),
-  },
-  {
-    id: "2",
-    name: "Desenvolvedor Front-end",
-    company: "TechCorp",
-    createdAt: new Date(),
-  },
-  {
-    id: "3",
-    name: "Engenheiro de Software",
-    company: "InnovaTech",
-    createdAt: new Date(),
-  },
-  {
-    id: "4",
-    name: "Designer UX/UI",
-    company: "Creative Studio",
-    createdAt: new Date(),
-  },
-  {
-    id: "5",
-    name: "Analista de Dados",
-    company: "DataWorks",
-    createdAt: new Date(),
-  },
-];
-
 export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return;
+  }
+
+  const myApplications = await db
+    .select({
+      id: application.id,
+      name: application.name,
+      company: application.company,
+      createdAt: application.createdAt,
+      updatedAt: application.updatedAt,
+    })
+    .from(application)
+    .where(eq(application.userId, session.user.id));
+
   return (
     <MaxWidth>
       <div className="flex justify-between gap-6 mb-4">
@@ -54,7 +44,7 @@ export default async function Page() {
 
       <div>
         <ol className="grid grid-cols-3 gap-4">
-          {applicationMock.map((item) => (
+          {myApplications.map((item) => (
             <li key={item.id}>
               <Link href={`/applications/${item.id}`}>
                 <Card className="group">
