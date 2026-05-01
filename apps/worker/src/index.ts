0da718fb42f94008911fb90ff37d8ba34c56e905
Tito Lucca Oliveira <titoluccao@gmail.com>
2026-05-01 17:11:14 -0300

    feat: adicionar worker

---
+++ apps/worker/src/index.ts
@@ -0,0 +1,29 @@
+import { env } from "@prosopopeia/env/server";
+import { Worker } from "bullmq";
+
+const worker = new Worker(
+  "prosopopeia-queue",
+  async (job) => {
+    console.log(`Processing job ${job.id} with data:`, job.data);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return { status: "completed" };
+  },
+  {
+    connection: {
+      host: env.UPSTASH_REDIS_REST_URL,
+      password: env.UPSTASH_REDIS_REST_TOKEN,
+      tls: {},
+      port: 6379,
+    },
+  },
+);
+
+worker.on("completed", (job) => {
+  console.log(`${job.id} completou!`);
+});
+
+worker.on("failed", (job, err) => {
+  console.log(`${job?.id} falhou com a mensagem ${err.message}`);
+});
+
+console.log(`Worker iniciado.`);
